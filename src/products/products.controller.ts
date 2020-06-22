64e851f03f9c98f3730340d949dcf067c3754966
Daniel Mwakanema <danielmwakanema95@gmail.com>
2020-06-22 13:00:22 +0200

    refactor(Controllers): Use products service
    - Moved resolving code to product service to keep controller thin

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,6 +1,9 @@
 import { Controller, Get, Param, Query, UseInterceptors, CacheInterceptor } from '@nestjs/common';
 import { ProductQuery } from 'src/common/interfaces/product-query.interface';
 import { ProductsService } from './products.service';
+import { ProductDto } from 'src/common/dtos/product.dto';
+import { ProductsDto } from 'src/common/dtos/products.dto';
+import { ProductsQuery } from 'src/common/interfaces/products-query.interface';
 
 @Controller('products')
 @UseInterceptors(CacheInterceptor)
@@ -8,12 +11,12 @@ export class ProductsController {
   constructor(private readonly service: ProductsService) {}
 
   @Get()
-  findAll(): Promise<void> {
-    return Promise.resolve();
+  findAll(@Query() query: ProductsQuery): Promise<ProductsDto> {
+    return this.service.findAll(query);
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string, @Query() query: ProductQuery): Promise<void> {
-    return Promise.resolve();
+  findOne(@Param('id') id: string, @Query() query?: ProductQuery): Promise<ProductDto> {
+    return this.service.findOne(id, query);
   }
 }
