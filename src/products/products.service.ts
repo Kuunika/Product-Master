ac5bbc36482bdced81cf3b9317b8e6c0776227eb
Daniel Mwakanema <danielmwakanema95@gmail.com>
2020-06-22 13:01:33 +0200

    feat(Products): + code to resolve product queries
    - Added code in product service to resolve product queries
    - Injected an OCL client
    - Added dummy OCL client to fulfill contracts

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,4 +1,60 @@
 import { Injectable } from '@nestjs/common';
+import { ProductsDto } from 'src/common/dtos/products.dto';
+import { ProductQuery } from 'src/common/interfaces/product-query.interface';
+import { ProductDto } from 'src/common/dtos/product.dto';
+import { ProductsQuery } from 'src/common/interfaces/products-query.interface';
 
 @Injectable()
-export class ProductsService {}
+export class ProductsService {
+  constructor(private readonly client: OclClient) { }
+
+  findAll(filters: ProductsQuery): Promise<ProductsDto> {
+    return this.client.findAll(filters);
+  }
+
+  findOne(id: string, filters: ProductQuery): Promise<ProductDto> {
+    return this.client.findOne(id, filters);
+  }
+}
+
+@Injectable()
+export class OclClient {
+  findAll(filters: ProductsQuery): Promise<ProductsDto> {
+    return Promise.resolve({
+      page: 1,
+      pageSize: 10,
+      totalPages: 20,
+      products: [
+        {
+          productCode: 'XXXX',
+          productName: 'XXXX',
+          mappings: [
+            {
+              systemName: 'XXXX',
+              systemProductCode: 'XXXX',
+              productName: 'XXXX'
+            }
+          ],
+          dateCreated: new Date(),
+          lastUpdated: new Date()
+        }
+      ]
+    });
+  }
+
+  findOne(id: string, filters: ProductQuery): Promise<ProductDto> {
+    return Promise.resolve({
+      productCode: 'XXXX',
+      productName: 'XXXX',
+      mappings: [
+        {
+          systemName: 'XXXX',
+          systemProductCode: 'XXXX',
+          productName: 'XXXX'
+        }
+      ],
+      dateCreated: new Date(),
+      lastUpdated: new Date()
+    })
+  }
+} 
