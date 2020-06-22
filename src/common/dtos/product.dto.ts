5c0f7b651e10196acc3f3bd429a562604d029b3f
Brett Onions <onionsllb@outlook.com>
2020-06-22 10:56:52 +0200

    Added Dtos, exceptions and interfaces to common folder

---
+++ src/common/dtos/product.dto.ts
@@ -0,0 +1,6 @@
+export interface ProductDto {
+    productCode: string;
+    productName: string;
+    dateCreated: Date;
+    lastUpdated: Date;
+}
---
+++ src/common/dtos/system-product.dto.ts
@@ -0,0 +1,5 @@
+export interface SystemProductDto {
+    systemName: string;
+    systemProductCode: string;
+    productName: string;
+}
