d6fdf281f4f7d6234714089c7066119141e62380
Brett Onions <onionsllb@outlook.com>
2020-06-22 15:02:28 +0200

    Created ocl-client service for getting products

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { OclClientModule } from './ocl-client/ocl-client.module';
 
 @Module({
-  imports: [],
+  imports: [OclClientModule],
   controllers: [AppController],
   providers: [AppService],
 })
