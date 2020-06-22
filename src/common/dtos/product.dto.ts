d632e711cca471304102683a05307098512d4955
Daniel Mwakanema <danielmwakanema95@gmail.com>
2020-06-22 12:58:26 +0200

    refactor(DTOs): Add mappings to product dto

--- src/common/dtos/product.dto.ts
+++ src/common/dtos/product.dto.ts
@@ -1,6 +1,9 @@
+import { SystemProductDto } from "./system-product.dto";
+
 export interface ProductDto {
     productCode: string;
     productName: string;
+    mappings: SystemProductDto[];
     dateCreated: Date;
     lastUpdated: Date;
 }
