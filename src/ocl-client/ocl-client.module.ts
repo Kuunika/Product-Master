d181b8da0cd94b85260f99c9fa98cefed3c9b980
Daniel Mwakanema <danielmwakanema95@gmail.com>
2020-06-23 11:29:13 +0200

    refactor(Modules): export OclClientService

--- src/ocl-client/ocl-client.module.ts
+++ src/ocl-client/ocl-client.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { OclClientService } from './ocl-client.service';
 
 @Module({
-  providers: [OclClientService]
+  providers: [OclClientService],
+  exports: [OclClientService]
 })
 export class OclClientModule {}
