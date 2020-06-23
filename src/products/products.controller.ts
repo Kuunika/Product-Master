7a211a3ceb0ba7b5eb18400c3514385ccb18b989
Daniel Mwakanema <danielmwakanema95@gmail.com>
2020-06-23 11:30:48 +0200

    refactor(Controllers): Change product return types
    - Changed return types of products to default OCL responses
    - TODO: reformat responses

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,22 +1,22 @@
 import { Controller, Get, Param, Query, UseInterceptors, CacheInterceptor } from '@nestjs/common';
 import { ProductQuery } from 'src/common/interfaces/product-query.interface';
 import { ProductsService } from './products.service';
-import { ProductDto } from 'src/common/dtos/product.dto';
-import { ProductsDto } from 'src/common/dtos/products.dto';
 import { ProductsQuery } from 'src/common/interfaces/products-query.interface';
+import { OclConcept } from 'src/common/interfaces/ocl-concept.interface';
+import { ListOfOclConcepts } from 'src/common/interfaces/list-of-ocl-concepts.interface';
 
 @Controller('products')
 @UseInterceptors(CacheInterceptor)
 export class ProductsController {
-  constructor(private readonly service: ProductsService) {}
+  constructor(private readonly service: ProductsService) { }
 
   @Get()
-  findAll(@Query() query: ProductsQuery): Promise<ProductsDto> {
+  findAll(@Query() query: ProductsQuery): Promise<ListOfOclConcepts> {
     return this.service.findAll(query);
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string, @Query() query?: ProductQuery): Promise<ProductDto> {
+  findOne(@Param('id') id: string, @Query() query?: ProductQuery): Promise<OclConcept> {
     return this.service.findOne(id, query);
   }
 }
