63f3f31702e40c537b122519ea45ba0f3faff672
Daniel Mwakanema <danielmwakanema95@gmail.com>
2020-06-23 13:07:21 +0200

    refactor(Services): add response format helpers
    - Added functions to map OCL responses into more useful forms

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -2,18 +2,50 @@ import { Injectable } from '@nestjs/common';
 import { ProductQuery } from 'src/common/interfaces/product-query.interface';
 import { ProductsQuery } from 'src/common/interfaces/products-query.interface';
 import { OclClientService } from 'src/ocl-client/ocl-client.service';
-import { OclConcept } from 'src/common/interfaces/ocl-concept.interface';
+import { OclConcept, Mapping } from 'src/common/interfaces/ocl-concept.interface';
 import { ListOfOclConcepts } from 'src/common/interfaces/list-of-ocl-concepts.interface';
+import { ProductDto } from 'src/common/dtos/product.dto';
+import { SystemProductDto } from 'src/common/dtos/system-product.dto';
+import { ProductsDto } from 'src/common/dtos/products.dto';
 
 @Injectable()
 export class ProductsService {
   constructor(private readonly service: OclClientService) { }
 
-  findAll(filters: ProductsQuery): Promise<ListOfOclConcepts> {
-    return this.service.getProducts(filters.page, filters.pageSize);
+  async findAll(filters: ProductsQuery): Promise<ProductsDto> {
+    const products = await this.service.getProducts(filters.page, filters.pageSize);
+    return this.toOclProducts(products);
   }
 
-  findOne(id: string, filters: ProductQuery): Promise<OclConcept> {
-    return this.service.getProductByCode(id, filters.system, filters.fhir);
+  async findOne(id: string, filters: ProductQuery): Promise<ProductDto> {
+    const product = await this.service.getProductByCode(id, filters.system, filters.fhir);
+    return this.toProduct(product);
+  }
+
+  private toProduct(concept: OclConcept): ProductDto {
+    return {
+      productCode: concept.uuid,
+      productName: concept.display_name,
+      mappings: concept.mappings.map(val => this.toSystemProduct(val)),
+      dateCreated: new Date(concept.created_on),
+      lastUpdated: new Date(concept.updated_on)
+    };
+  }
+
+  private toOclProducts(concepts: ListOfOclConcepts): ProductsDto {
+    return {
+      page: concepts.currentPage,
+      totalNumberOfPages: concepts.totalNumberOfPages,
+      totalNumberOfProducts: concepts.totalNumberOfConcepts,
+      products: concepts.concepts.map(val => this.toProduct(val))
+    }
+  }
+
+  private toSystemProduct(map: Mapping): SystemProductDto {
+    return {
+      systemName: map.source,
+      systemProductCode: map.external_id,
+      productName: map.to_concept_name
+    }
   }
 }
