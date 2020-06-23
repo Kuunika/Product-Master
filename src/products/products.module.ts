af717e6bc9da37dc30d75759730f835b6f397677
Daniel Mwakanema <danielmwakanema95@gmail.com>
2020-06-23 11:31:08 +0200

    refactor(Modules): Import OCL into Products

--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -1,10 +1,11 @@
 import { Module, CacheModule } from '@nestjs/common';
 import { ProductsController } from './products.controller';
 import { ProductsService } from './products.service';
+import { OclClientModule } from 'src/ocl-client/ocl-client.module';
 
 @Module({
-  imports: [CacheModule.register()],
+  imports: [CacheModule.register(), OclClientModule],
   controllers: [ProductsController],
   providers: [ProductsService]
 })
-export class ProductsModule {}
+export class ProductsModule { }
