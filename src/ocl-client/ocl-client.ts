59475fa4626214c98f78a82d6442c4fb2d403871
Daniel Mwakanema <danielmwakanema95@gmail.com>
2020-06-24 08:00:38 +0200

    refactor(OCLClient): Update to new exceptions

--- src/ocl-client/ocl-client.ts
+++ src/ocl-client/ocl-client.ts
@@ -1,14 +1,13 @@
 import * as axios from 'axios';
 import { OclConcept } from '../common/interfaces/ocl-concept.interface';
 import { apiConfig } from './api.config';
-import { SystemDoesNotExistException } from '../common/exceptions/system-does-not-exist.exception';
-import { HttpException, HttpStatus } from '@nestjs/common';
-import { ProductCodeDoesNotExistException } from '../common/exceptions/product-code-does-not-exist.exception';
 import { ListOfOclConcepts } from '../common/interfaces/list-of-ocl-concepts.interface';
-import { ProductDoesNotExistInTheSpecifiedSystemException } from '../common/exceptions/product-does-not-exist-in-the-specified-system.exception';
+import { ProductNotFoundException } from 'src/common/exceptions/product-code-does-not-exist.exception';
+import { SystemNotFoundException } from 'src/common/exceptions/system-does-not-exist.exception';
+import { ProductNotFoundInSystemException } from 'src/common/exceptions/product-does-not-exist-in-the-specified-system.exception';
+import { OclClientException } from 'src/common/exceptions/ocl-client.exception';
 
 export class OclClient {
-
     private axiosClient: axios.AxiosInstance;
     private masterRepo = process.env.MASTER_REPO;
 
@@ -16,25 +15,22 @@ export class OclClient {
         this.axiosClient = axios.default.create(apiConfig);
     }
 
-    async getProductFromOcl(productCode: string, system?: string, fhir?: boolean): Promise<OclConcept> {
+    async getProductFromOcl(productCode: string, system?: string): Promise<OclConcept> {
         if (system) this.checkIfSystemExists(system);
 
         const productFromOcl = await this.axiosClient.get<OclConcept>(`sources/${this.masterRepo}/concepts/${productCode}?includeMappings=true`, apiConfig)
-            .catch(() => { throw new ProductCodeDoesNotExistException() });
+            .catch(() => { throw new ProductNotFoundException() });
 
         if (system && productFromOcl) this.checkIfProductExistsInSystem(system, productFromOcl.data);
 
         return productFromOcl.data;
-
     }
 
     async checkIfSystemExists(system: string): Promise<void> {
         try {
             await this.axiosClient.get(`sources/${system}`, apiConfig);
         } catch (error) {
-            if (error && error.response) {
-                throw new SystemDoesNotExistException();
-            }
+            throw new SystemNotFoundException();
         }
     }
 
@@ -42,12 +38,10 @@ export class OclClient {
         const systemsWhereProductExist = returnProductFromOCL.mappings.map(mapping => mapping.to_source_url.split('/').filter(element => element != '')[3]);
         const productExistsInSystem = systemsWhereProductExist.find(existingSystem => existingSystem === system);
 
-        if (!productExistsInSystem) throw new ProductDoesNotExistInTheSpecifiedSystemException();
-
+        if (!productExistsInSystem) throw new ProductNotFoundInSystemException();
     }
 
     async getAllProductsFromOcl(pageNumber: number, pageSize: number): Promise<ListOfOclConcepts> {
-
         try {
             const productsFromOcl = await this.axiosClient.get<OclConcept[]>(`sources/${this.masterRepo}/concepts?limit=${pageSize}&page=${pageNumber}`);
             return {
@@ -56,12 +50,8 @@ export class OclClient {
                 totalNumberOfConcepts: productsFromOcl.headers.num_found,
                 totalNumberOfPages: Math.ceil(productsFromOcl.headers.num_found / pageSize),
             }
-
         } catch (error) {
-            if (error && error.response) {
-                throw new HttpException('Server Error, Try Again Later', HttpStatus.INTERNAL_SERVER_ERROR);
-            }
-
+            throw new OclClientException();
         }
     }
 }
