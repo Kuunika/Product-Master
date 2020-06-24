a475c1e3b9f854525e749c4142871fb41af1c8f4
Daniel Mwakanema <danielmwakanema95@gmail.com>
2020-06-24 09:17:45 +0200

    refactor(Services): Use new formatting utils

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -2,11 +2,9 @@ import { Injectable } from '@nestjs/common';
 import { ProductQuery } from 'src/common/interfaces/product-query.interface';
 import { ProductsQuery } from 'src/common/interfaces/products-query.interface';
 import { OclClientService } from 'src/ocl-client/ocl-client.service';
-import { OclConcept, Mapping } from 'src/common/interfaces/ocl-concept.interface';
-import { ListOfOclConcepts } from 'src/common/interfaces/list-of-ocl-concepts.interface';
 import { ProductDto } from 'src/common/dtos/product.dto';
-import { SystemProductDto } from 'src/common/dtos/system-product.dto';
 import { ProductsDto } from 'src/common/dtos/products.dto';
+import { toProduct, toOclProducts } from '../utils/formatters';
 
 @Injectable()
 export class ProductsService {
@@ -14,38 +12,11 @@ export class ProductsService {
 
   async findAll(filters: ProductsQuery): Promise<ProductsDto> {
     const products = await this.service.getProducts(filters.page, filters.pageSize);
-    return this.toOclProducts(products);
+    return toOclProducts(products);
   }
 
   async findOne(id: string, filters: ProductQuery): Promise<ProductDto> {
     const product = await this.service.getProductByCode(id, filters.system);
-    return this.toProduct(product);
-  }
-
-  private toProduct(concept: OclConcept): ProductDto {
-    return {
-      productCode: concept.uuid,
-      productName: concept.display_name,
-      mappings: concept.mappings.map(val => this.toSystemProduct(val)),
-      dateCreated: new Date(concept.created_on),
-      lastUpdated: new Date(concept.updated_on)
-    };
-  }
-
-  private toOclProducts(concepts: ListOfOclConcepts): ProductsDto {
-    return {
-      page: concepts.currentPage,
-      totalNumberOfPages: concepts.totalNumberOfPages,
-      totalNumberOfProducts: concepts.totalNumberOfConcepts,
-      products: concepts.concepts.map(val => this.toProduct(val))
-    }
-  }
-
-  private toSystemProduct(map: Mapping): SystemProductDto {
-    return {
-      systemName: map.source,
-      systemProductCode: map.external_id,
-      productName: map.to_concept_name
-    }
+    return toProduct(product);
   }
 }
