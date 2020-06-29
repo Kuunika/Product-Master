97cb95c0a70acdca3f2acf5d6baead70649f6362
Daniel Mwakanema <danielmwakanema95@gmail.com>
2020-06-29 12:22:04 +0200

    refactor(Modules): Drop ocl module from App

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,9 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductsModule } from './products/products.module';
-import { OclClientModule } from './ocl-client/ocl-client.module';
 
 @Module({
-  imports: [OclClientModule, ProductsModule],
+  imports: [ProductsModule],
   controllers: [AppController],
   providers: [AppService],
 })
