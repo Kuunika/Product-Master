a76f7eb88a3b1b13b70cc69bfc662533452b42e9
Daniel Mwakanema <danielmwakanema95@gmail.com>
2020-11-30 10:22:01 +0200

    refactor(AppModule): register logger

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,38 @@
 import { Module } from '@nestjs/common';
+import { transports as Transports } from 'winston';
+import { WinstonModule, WinstonModuleOptions } from 'nest-winston';
 import { AppService } from './app.service';
 import { ProductsModule } from './products/products.module';
 import { FhirProductsModule } from './fhir-products/fhir-products.module';
+import { defaultLogFormat } from './common/utils/os';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { OCLModule } from './ocl/ocl.module';
 
 @Module({
-  imports: [ProductsModule, FhirProductsModule],
+  imports: [
+    WinstonModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: function (service: ConfigService): WinstonModuleOptions {
+        const transports: Array<Transports.ConsoleTransportInstance | Transports.FileTransportInstance> = [
+          new Transports.File({ filename: 'logs/error.log', level: 'error', format: defaultLogFormat() }),
+          new Transports.File({ filename: 'logs/combined.log', format: defaultLogFormat() })
+        ];
+        if (service.get<string>('NODE_ENV') !== 'production') {
+          transports.push(new Transports.Console({ format: defaultLogFormat() }));
+        }
+        return {
+          level: 'info',
+          format: defaultLogFormat(),
+          defaultMeta: { service: 'Product Master' },
+          transports: transports
+        }
+      },
+      inject: [ConfigService]
+    }),
+    ProductsModule,
+    FhirProductsModule,
+    OCLModule
+  ],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
