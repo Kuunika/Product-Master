faa6e8035aa3e15af374d45601f6647d638c393b
Brett Larry Onions <46473797+brett-onions@users.noreply.github.com>
2021-05-13 09:55:33 +0200

    created new case converter pipe

---
+++ src/pipes/system-name-converter.pipe.spec.ts
@@ -0,0 +1,7 @@
+import { SystemNameConverterPipe } from './system-name-converter.pipe';
+
+describe('SystemNameConverterPipe', () => {
+  it('should be defined', () => {
+    expect(new SystemNameConverterPipe()).toBeDefined();
+  });
+});
